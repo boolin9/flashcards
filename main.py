e6b789656e6ba6ea8fd88a912ddcf44adf31a7c9
andrewhought <80929614+boolin9@users.noreply.github.com>
2022-08-07 10:05:53 -0700

    Final product, will reorganize with OOP

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from tkinter import *
+import csv
+import random
 
 BACKGROUND_COLOR = '#B1DDC6'
 TEXT_COLOR = '#34444D'
@@ -7,32 +9,81 @@ root = Tk()
 root.title('tkFlash')
 root.config(background=BACKGROUND_COLOR, padx=50, pady=50)
 
+with open('french_words.csv') as file:
+    reader = csv.DictReader(file)
+    word_dict = {rows['French']:rows['English'] for rows in reader}
+
+dict_copy = word_dict.copy()
+total_words = (len(word_dict.keys()))
+
+
+def get_random_word():
+    global french_word
+    global english_word
+    random_dict_item = random.sample(dict_copy.items(), 1)
+    french_word = random_dict_item[0][0]
+    english_word = random_dict_item[0][1]
+
 
 def turn_card():
     global card_state
+    global score_text
+    global french_text
+    global english_text
+    global french_word
+    global english_word
+    global front_card
     card.delete('all')
     
+    
     if card_state:
-        card.create_image(400, 263, image=front_image)
+        front_card = card.create_image(400, 263, image=front_image)
         card.create_text(400, 150, text="French", fill=TEXT_COLOR, font=('Helvetica', 40, 'italic'))
-        card.create_text(400, 263, text="Word", fill=TEXT_COLOR, font=('Helvetica', 60, 'bold'))
-        card.create_text(400, 430, text="Word", fill=BACKGROUND_COLOR, font=('Helvetica', 20, 'bold'))
+        french_text = card.create_text(400, 263, text=f"{french_word}", fill=TEXT_COLOR, font=('Helvetica', 60, 'bold'))
+        score_text = card.create_text(400, 430, text=f'{score}/{total_words}', fill=TEXT_COLOR, font=('Helvetica', 20, 'bold'))
+
     else:
         card.create_image(400, 263, image=back_image)
-        card.create_text(400, 263, text="French", fill='white', font=('Helvetica', 60, 'bold'))
+        card.create_text(400, 150, text="English", fill=TEXT_COLOR, font=('Helvetica', 40, 'italic'))
+        english_text = card.create_text(400, 263, text=f"{english_word}", fill=TEXT_COLOR, font=('Helvetica', 60, 'bold'))
+        score_text = card.create_text(400, 430, text=f'{score}/{total_words}', fill=TEXT_COLOR, font=('Helvetica', 20, 'bold'))
 
     card_state = not card_state
 
 
 def correct_answer():
-    pass
-
+    global card_state
+    global score
+    global score_text
+    global french_text
+    global english_text
+    global french_word
+    global english_word
+    if score < total_words:
+        score += 1
+        get_random_word()
+        card_state = True
+        turn_card()
+        del dict_copy[french_word]
+            
 
 def wrong_answer():
-    pass
+    global card_state
+    global score
+    global score_text
+    global french_text
+    global english_text
+    global french_word
+    global english_word
+    global random_dict_item
+    if score < total_words:
+        get_random_word()
+        card_state = True
+        turn_card()
 
 
-card_state = True 
+card_state = True
+score = 0
 
 card = Canvas(root, width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 card.grid(column=0, row=0, columnspan=3)
@@ -43,15 +94,16 @@ rbutton_image = PhotoImage(file='images/right.png')
 wbutton_image = PhotoImage(file='images/wrong.png')
 tbutton_image = PhotoImage(file='images/turn.png')
 
-right_button = Button(image=rbutton_image, highlightbackground=BACKGROUND_COLOR)
+right_button = Button(image=rbutton_image, highlightbackground=BACKGROUND_COLOR, command=correct_answer)
 right_button.grid(column=0, row=1)
 
-wrong_button = Button(image=wbutton_image, highlightbackground=BACKGROUND_COLOR)
+wrong_button = Button(image=wbutton_image, highlightbackground=BACKGROUND_COLOR, command=wrong_answer)
 wrong_button.grid(column=2, row=1)
 
 turn_button = Button(image=tbutton_image, bg=BACKGROUND_COLOR, highlightbackground=BACKGROUND_COLOR, command=turn_card)
 turn_button.grid(column=1, row=1)
 
+get_random_word()
 turn_card()
 
 
